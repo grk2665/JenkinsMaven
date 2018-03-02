344490ba977bf317a6a037183fecee6bf85117ea
ravi <rrk736@gmail.com>
2018-03-02 14:04:13 +0530

    commit

---
+++ src/main/java/com/emids/jenkinsMaven/HelloWorldMaven.java
@@ -0,0 +1,20 @@
+package com.emids.jenkinsMaven;
+
+import java.util.Date;
+import org.apache.commons.lang.time.DateFormatUtils;
+
+public class HelloWorldMaven {
+
+		public static void main(String[] args) {
+			System.out.println("Hello Wold from Maven.....Today is :  " + getToday());
+		}
+		
+		public static String getToday() {
+			String today = DateFormatUtils.format(new Date(), "dd-MMM-yyyy");
+			return today;
+		}
+
+	}
+
+
+
---
+++ src/test/java/com/emids/jenkinsMaven/HelloWorldMavenTest.java
@@ -0,0 +1,19 @@
+package com.emids.jenkinsMaven;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class HelloWorldMavenTest {
+	
+	
+	@Test
+	public void testGetToday() {
+		String actual = HelloWorldMaven.getToday();
+		String expected = new SimpleDateFormat("dd-MMM-yyyy").format(new Date());
+		assertEquals(expected,actual);
+	}
+
+}
